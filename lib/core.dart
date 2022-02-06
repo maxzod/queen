2a8f02e46082f62f2553884b769b25e085e41e0b
maxzod66 <maxzod66@gmail.com>
2022-02-06 13:54:21 +0200

    export prefs facade

--- lib/core.dart
+++ lib/core.dart
@@ -3,6 +3,7 @@ library queen;
 export 'package:flutter_readable/flutter_readable.dart';
 export 'package:queen_validators/queen_validators.dart';
 
+export 'core/helpers/prefs.dart';
 export 'core/helpers/url_luncher.dart';
 export 'core/widgets/queen_builder.dart';
 export 'debug_bar.dart';
